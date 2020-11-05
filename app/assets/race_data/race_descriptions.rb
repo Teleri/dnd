207225e4a1287b020d527672bffcc52fab610967
Michael Knott <knottweb@gmail.com>
2020-11-05 17:32:49 +0000

    banner image

---
+++ app/assets/race_data/race_descriptions.rb
@@ -0,0 +1,178 @@
+"Gnome, Half-Elf, Halfling, Half-Orc, Human, Tiefling,
+  Orc of Exandria, Leonin, Satyr, Aarakocra, Genasi, Goliath, Aasimar, Bugbear, Firbolg, 
+  Goblin, Hobgoblin, Kenku, Kobold, Lizardfolk, Orc, Tabaxi, Triton, Yuan-ti Pureblood, 
+  Feral Tiefling, Tortle, Changeling, Kalashtar, Orc of Eberron, Shifter, Warforged, 
+  Gith, Centaur, Loxodon, Minotaur, Simic Hybrid, Vedalken, Verdan, Locathah"
+
+"Racial Description
+Dragonborn look very much like dragons standing erect in humanoid form, though they lack wings or a tail.
+Racial Traits
++2 Strength, +1 Charisma, Draconic Ancestry, Breath Weapon, Damage Resistance"
+"Racial Description
+Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal.
+Racial Traits
++2 Constitution, Darkvision, Dwarven Resilience, Dwarven Combat Training, Stonecunning"
+"Racial Description
+Elves are a magical people of otherworldly grace, living in the world but not entirely part of it.
+Racial Traits
++2 Dexterity, Darkvision, Keen Senses, Fey Ancestry, Trance"
+"Racial Description
+A gnome’s energy and enthusiasm for living shines through every inch of his or her tiny body.
+Racial Traits
++2 Intelligence, Darkvision, Gnome Cunning"
+"Racial Description
+Half-elves combine what some say are the best qualities of their elf and human parents.
+Racial Traits
++2 Charisma, +1 to Two Other Ability Scores, Darkvision, Fey Ancestry, Skill Versatility"
+"Racial Description
+The diminutive halflings survive in a world full of larger creatures by avoiding notice or, barring that, avoiding offense.
+Racial Traits
++2 Dexterity, Lucky, Brave, Halfling Nimbleness"
+"Racial Description
+Half-orcs’ grayish pigmentation, sloping foreheads, jutting jaws, prominent teeth, and towering builds make their orcish heritage plain for all to see.
+Racial Traits
++2 Strength, +1 Constitution, Darkvision, Menacing, Relentless Endurance, Savage Attacks"
+"Racial Description
+Humans are the most adaptable and ambitious people among the common races. Whatever drives them, humans are the innovators, the achievers, and the pioneers of the worlds.
+Racial Traits
++1 to All Ability Scores, Extra Language"
+"Racial Description
+To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling.
+Racial Traits
++2 Charisma, +1 Intelligence, Darkvision, Hellish Resistance, Infernal Legacy"
+"Racial Description
+Orcs are one of Exandria’s youngest races, and are said to have been born from elves seared by the blood of Gruumsh, the Ruiner.
+Racial Traits
++2 Strength, +1 Constitution, Darkvision, Aggressive, Powerful Build, Primal Intuition"
+"Racial Description
+Nomadic, lion-like humanoids who rarely interact with other peoples, having all they need in their shimmering homeland. 
+Racial Traits
++2 Constitution, +1 Strength, Darkvision, Claws,  Hunter's Instincts, Daunting Roar."
+"Racial Description
+Satyrs embody a fusion of humanoid civilization with the freedom of wild beasts.
+Racial Traits
++2 Charisma, +1 Dexterity, Fey, Ram, Magic Resistance, Mirthful Leaps, Reveler."
+"Racial Description
+Sequestered in high mountains atop tall trees, the aarakocra, sometimes called birdfolk, evoke fear and wonder.
+Racial Traits
++2 Dexterity, +1 Wisdom, Flight, Talons"
+"Racial Description
+Genasi carry the power of the elemental planes of air, earth, fire, and water in their blood.
+Racial Traits
++2 Constitution, Air, Earth, Fire, Water Subraces"
+"Racial Description
+Strong and reclusive, every day brings a new challenge to a goliath.
+Racial Traits
++2 Strength, +1 Constitution, Natural Athlete, Stone's Endurance, Powerful Build, Mountain Born"
+"Racial Description
+Aasimar are placed in the world to serve as guardians of law and good. Their patrons expect them to strike at evil, lead by example, and further the cause of justice.
+Racial Traits
++2 Charisma, Darkvision, Celestial Resistance, Healing Hands, Light Bearer"
+"Racial Description
+Bugbears feature in the nightmare tales of many races — great, hairy beasts that creep through the shadows as quiet as cats.
+Racial Traits
++2 Strength, +1 Dexterity, Darkvision, Long-Limbed, Powerful Build, Sneaky, Surprise Attack"
+"Racial Description
+Firbolg tribes cloister in remote forest strongholds, preferring to spend their days in quiet harmony with the woods.
+Racial Traits
++2 Wisdom, +1 Strength, Firbolg Magic, Hidden Step, Powerful Build, Speech of Beast and Leaf"
+"Racial Description
+Goblins occupy an uneasy place in a dangerous world, and they react by lashing out at any creatures they believe they can bully.
+Racial Traits
++2 Dexterity, +1 Constitution, Darkvision, Fury of the Small, Nimble Escape"
+"Racial Description
+War is the lifeblood of hobgoblins. Its glories are the dreams that inspire them. Its horrors don’t feature in their nightmares.
+Racial Traits
++2 Constitution, +1 Intelligence, Darkvision, Martial Training, Saving Face"
+"Racial Description
+Haunted by an ancient crime that robbed them of their wings, the kenku wander the world as vagabonds and burglars who live at the edge of human society.
+Racial Traits
++2 Dexterity, +1 Wisdom, Expert Forgery, Kenku Training, Mimicry"
+"Racial Description
+Kobolds are typically timid and shy away from conflict, but they are dangerous and vicious if cornered.
+Racial Traits
++2 Dexterity, Darkvision, Grovel, Cower, and Beg, Pack Tactics, Sunlight Sensitivity"
+"Racial Description
+Lizardfolk possess an alien and inscrutable mindset, their desires and thoughts driven by a different set of basic principles than those of warm-blooded creatures.
+Racial Traits
++2 Constitution, +1 Wisdom, Bite, Cunning Artisan, Hold Breath, Hunter's Lore, Natural Armor, Hungry Jaws"
+"Racial Description
+Orcs live a life that has no place for weakness, and every warrior must be strong enough to take what is needed by force.
+Racial Traits
++2 Strength, +1 Constitution, Darkvision, Aggressive, Primal Intuition, Powerful Build"
+"Racial Description
+Hailing from a strange and distant land, wandering tabaxi are catlike humanoids driven by curiosity to collect interesting artifacts, gather tales and stories, and lay eyes on all the world’s wonders.
+Racial Traits
++2 Dexterity, +1 Charisma, Darkvision, Feline Agility, Cat's Claws, Cat's Talent"
+"Racial Description
+Long-established guardians of the deep ocean floor, in recent years the noble tritons have become increasingly active in the world above.
+Racial Traits
++1 Strength, +1 Constitution, +1 Charisma, Amphibious, Control Air and Water, Emissary of the Sea, Guardians of the Depths"
+"Racial Description
+The serpent creatures known as yuan-ti are all that remains of an ancient, decadent human empire.
+Racial Traits
++2 Charisma, +1 Intelligence, Darkvision, Innate Spellcasting, Magic Resistance, Poison Immunity"
+"Racial Description
+To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling.
+Racial Traits
++2 Dexterity, +1 Intelligence, Darkvision, Hellish Resistance, Infernal Legacy"
+"Racial Description
+What many tortles consider a simple life, others might call a life of adventure. They are nomad survivalists eager to explore the wilderness.
+Racial Traits
++2 Strength, +1 Wisdom, Claws, Hold Breath, Natural Armor, Shell Defense, Survival Instinct"
+"Racial Description
+A changeling can shift its face and form with a thought as a form of artistic and emotional expression.
+Racial Traits
++2 Charisma, +1 an Ability Score of your choice, Shapechanger, Changeling Instincts"
+"Racial Description
+The kalashtar are a compound people created from the union of humanity and renegade spirits from the plane of dreams.
+Racial Traits
++2 Wisdom, +1 Charisma, Dual Mind, Mental Discipline, Mind Link, Severed from Dreams"
+"Racial Description
+Thousands of years before humans came to Khorvaire, the land was dominated by wild orcs and goblinoids of the Dhakaani Empire.
+Racial Traits
++2 Strength, +1 Constitution, Darkvision, Aggressive, Powerful Build, Primal Intuition"
+"Racial Description
+Shifters - sometimes called the weretouched - are humanoids with a bestial aspect.
+Racial Traits
+Darkvision, Shifting"
+"Racial Description
+Warforged are made from wood and metal, but they can feel pain and emotion. Built as weapons, they must now find a purpose beyond war.
+Racial Traits
++2 Constitution, +1 to One Other Ability Score, Constructed Resilience, Sentry's Rest, Integrated Protection, Specialized Design"
+"Racial Description
+Long ago, the gith rose up to overthrow the mind flayers that held them in servitude, but two factions arose that remain bitter enemies today.
+Racial Traits
++1 Intelligence, Githyanki or Githzerai Traits"
+"Racial Description
+The upper bodies of centaurs are comparable to human torsos in size, and their lower equine bodies average about 4 feet tall at the withers. They fill similar roles as cavalry warriors, messengers, outriders, and scouts.
+Racial Traits
++2 Strength, +1 Wisdom, Fey, Charge, Hooves, Equine Build, Survivor"
+"Racial Description
+Humanoid elephants, loxodons are strong, calm, and wise.
+Racial Traits
++2 Constitution, +1 Wisdom, Powerful Build, Loxodon Serenity, Natural Armor, Trunk, Keen Smell"
+"Racial Description
+The minotaurs of Ravnica are strong in body, dedication, and courage. They are at home on the battlefield, willing to fight for their various causes.
+Racial Traits
++2 Strength, +1 Constitution, Horns, Goring Rush, Hammering Horns, Imposing Presence"
+"Racial Description
+The Simic Combine uses magic to transfer the traits of animals into humans, elves, and vedalken.
+Racial Traits
++2 Constitution, +1 One Other Ability Score, Darkvision, Animal Enhancement"
+"Racial Description
+The blue-skinned vedalken strive for perfection above all else.
+Racial Traits
++2 Intelligence, +1 Wisdom, Vedalken Dispassion, Tireless Precision, Partially Amphibious"
+"Racial Description
+The verdan owe their existence to chaos - doing their best to find their way in an unfamiliar world..
+Racial Traits
++1 Constitution, +2 Charisma, Black Blood Healing, Limited Telepathy, Persuasive, Telepathic Insight"
+"Racial Description
+These resilient and proud fish-folk have endured war, slavery, and mistreatment at the hands of other aquatic creatures.
+Racial Traits
++2 Strength, +1 Dexterity, Natural Armor, Observant & Athletic, Leviathan Will, Limited Amphibiousness"
+"Racial Description
+Your grung character has an assortment of inborn abilities, part and parcel of grung nature.
+Racial Traits
++2 Dexterity, +1 Constitution, Arboreal Alertness, Amphibious, Poison Immunity, Poisonous Skin, Standing Leap, Water Dependency."
