7480d1f6099e1067c86df2dd61ce2f081bbe1117
Michael Knott <knottweb@gmail.com>
2020-11-05 15:12:02 +0000

    Tweaked user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show edit update]
+
   def show
+  end
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
