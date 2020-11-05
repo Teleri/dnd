a5119c42028e75bbc8a0841c70e538b2b0c3cbe5
Michael Knott <knottweb@gmail.com>
2020-11-05 13:50:18 +0000

    copy of previous project

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,60 @@
+class CharactersController < ApplicationController
+  before_action :find_user, only: %i[new create show edit update]
+  before_action :find_character, only: %i[show edit update destroy]
+
+  def index
+    @characters = Character.all
+  end
+
+  def show
+    # find_character
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    @character.user = @user
+    if @character.save
+      redirect_to user_character_path(@user, @character)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    # find_character
+  end
+
+  def update
+    if @character.update(character_params)
+      redirect_to user_character_path(@character)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @character.destroy
+      redirect_to characters_path
+    else
+      redirect_to character_path(@character)
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def find_character
+    @character = Character.find(params[:id])
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :race, :character_class, :age, :height, :weight, :alignment, :background, :gender)
+  end
+end
